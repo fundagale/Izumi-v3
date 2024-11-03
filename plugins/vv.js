706f1bdc2eb620b1ad830f2b8c8f8d8e61cba803
fundagale <lucifer5881c@gmail.com>
2024-11-04 01:21:10 +0530

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -6,13 +6,11 @@ izumi(
     desc: "Forwards The View once messsage",
     type: "downloader",
   },
-  async (message, match) => {
-		if (!message.reply_message)
-			return await message.send('Reply to a message');
-		await message.forwardMessage(message.sender, message.quoted.data);
+		
   async (message, match) => {
   	if(!message.quoted) return;
     let buff = await message.quoted.download("buffer");
-    return await message.sendFile(buff);
+	  await message.forwardMessage(message.sender, message.quoted.data);
+    return await;
   }
 );
