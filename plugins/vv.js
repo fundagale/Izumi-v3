dcf362f8e14263cfd7b91caa2e987c43ccc7374d
fundagale <lucifer5881c@gmail.com>
2024-11-04 15:29:34 +0530

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -1,16 +1,24 @@
-const { izumi,mode } = require('../lib/');
+const { izumi, mode } = require('../lib/');
 izumi(
   {
     pattern: "vv",
     fromMe: mode,
-    desc: "Forwards The View once messsage",
+    desc: "Forwards the view-once message to your DM",
     type: "downloader",
   },
-		
   async (message, match) => {
-  	if(!message.quoted) return;
-    let buff = await message.quoted.download("buffer");
-	  await message.forwardMessage(message.sender, message.quoted.data);
-    return await;
+    try {
+      let buff = await message.quoted.download("buffer");
+await message.client.sendMessage(message.sender, {
+        image: buff,
+        viewOnce: false
+      });
+await message.client.sendMessage(message.sender, {
+        video: buff,
+        viewOnce: false
+      });
+    } catch (error) {
+      console.error("Error forwarding view-once message:", error);
+    }
   }
 );
